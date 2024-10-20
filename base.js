8e815e34e5114ecb6d70f6e290cb89d9db286c1a
Allam235 <106848509+Allam235@users.noreply.github.com>
2024-10-19 20:23:21 -0400

    Add files via upload

--- base.js
+++ base.js
@@ -1,68 +1,3 @@
- 
-//   async function setUpArrays(){
-//     let url = "https://api.aviationstack.com/v1/flights?access_key=e4aef054101ea4a81119444431b60b03";
-//     console.log(URL);
-//     let response = await fetch(URL);
-//     let responseJson = await response.json();
-//     console.log(responseJson);
-//     var array;
-    
-//     let length = responseJson[0].data.length;
-//     for(let i = 0; i < length; i++){
-//         for(let j = 0; 4; j++){
-//             if(j == 0){
-//                 array[i][j] = responseJson[0].data[i].flight_date;
-//             }
-//             else if(j == 1){
-//                 array[i][j] = responseJson[0].data[i].departure.airport;
-//             }
-//             else if(j == 2){
-//                 array[i][j] = responseJson[0].data[i].arrival.airport;
-//             }
-//             else{
-//                 array[i][j] = responseJson[0].data[i].airline.name;
-//             }
-//         }
-//     }
-
-//     let questionOneDiv = document.querySelector("#question1");
-//     questionOneDiv.innerHTML += "What airport(s) would you like to depart from?\n";
-//     const departDiv = document.createElement("div");
-//     departDiv.classList.add("property");
-//     let questionTwoDiv = document.querySelector("#question2");
-//     questionTwoDiv.innerHTML += "What airport(s) would you like to arrive in?\n";
-//     const arriveDiv = document.createElement("div");
-//     arriveDiv.classList.add("property");
-//     let questionThreeDiv = document.querySelector("#question3");
-//     questionThreeDiv.innerHTML += "What date(s) would you like to fly?\n";
-//     const dateDiv = document.createElement("div");
-//     dateDiv.classList.add("property");
-//     let questionFourDiv = document.querySelector("#question4");
-//     questionFourDiv.innerHTML += "What airline(s) would you like to fly with?";
-//     const airlineDiv = document.createElement("div");
-//     airlineDiv.classList.add("property");
-    
-
-    
-    
-
-//     for(let k = 0; k < length; k++){
-//         let place1 = responseJson[0].data[k].departure.airport;
-//         let place2 = responseJson[0].data[k].arrival.airport;
-//         let date = responseJson[0].data[k].flight_date;
-//         let airline = responseJson[0].data[k].airline.name;
-//         let display1 = "<button onClick = dispOne("+place1+")>" + place1 + "</button>";
-//         let display2 = "<button onClick = dispTwo("+place2+")>" + place2 + "</button>";
-//         let display3 = "<button onClick = dispThree("+date+")>" + date + "</button>";
-//         let display4 = "<button onClick = dispFour("+airline+")>" + airline + "</button>";
-//         departDiv.innerHTML += display1;
-//         arriveDiv.innerHTML += display2;
-//         dateDiv.innerHTML += display3;
-//         airlineDiv.innerHTMl += display4;
-//     }
-
-// }
-
 const twodArray = [
     [
         "2024-10-20",
@@ -798,16 +733,21 @@ async function initQ(){
     
     //parsedArray  = twodArray;
     const uniqueParsedArray = [];
-    for (let col = 0; col < parsedArray[0].length; col++) {
-        const uniqueValues = new Set();
-        for (const row of parsedArray) {
-            if(row[col] !== null){
-            //const value = row[col].replaceAll(" ", "");//removes all spaces within value
-            uniqueValues.add(row[col]);
-            }
+
+// Loop through each column
+for (let col = 0; col < parsedArray[0].length; col++) {
+    const uniqueValues = new Set();
+
+    // Loop through each row in the current column
+    for (const row of parsedArray) {
+        if (row[col] !== null && row[col] !== 'empty') {
+            uniqueValues.add(row[col]); // Add non-null value to the Set
+        }
     }
-    uniqueParsedArray.push(Array.from(uniqueValues));
+    // Convert Set to Array, sort it alphabetically, and push to result array
+    uniqueParsedArray.push(Array.from(uniqueValues).sort());
 }
+
 
     console.log(uniqueParsedArray);
 
@@ -1101,7 +1041,7 @@ function generateFlights(){
         flightDiv.innerHTML += "There are no flights which have that departure and arrival. Please try again with broader answers" + "<br>";
     }
     else{
-        flightDiv.innerHTML += "Here are the flight(s) found which best meet your specifications: \n";
+        flightDiv.innerHTML += "Here are the flight(s) found which best meet your specifications: <br>";
         if(firstScore > 7){
             flightDiv.innerHTML += "1. Flight number " + parsedArray[firstPos][1] + " leaves from " + parsedArray[firstPos][4] + " and arrives at " + parsedArray[firstPos][3] + " on " + parsedArray[firstPos][0] + " traveling by " + parsedArray[firstPos][2] + "<br>";
         }
